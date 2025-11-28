285f44842f779d722066f5e758e4a58ed64cb95d
root <cmlowerence2777@gmail.com>
2025-11-28 10:15:33 +0000

    SEO

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppWrapper from './App';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/react'; // Import this
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { HelmetProvider } from 'react-helmet-async'
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
+    <HelmetProvider>
     <AppWrapper />
     <Analytics />
-    <SpeedInsights /> {/* Add this line */}
+    <SpeedInsights />
+    <HelmetProvider/>
   </React.StrictMode>
 );
